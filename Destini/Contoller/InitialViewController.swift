2fe7e1e28def82fd7dbcad7a50a519aa9f243906
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-08 12:56:49 +0300

    final

--- Destini/Contoller/InitialViewController.swift
+++ Destini/Contoller/InitialViewController.swift
@@ -13,13 +13,29 @@ class InitialViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
-        // Do any additional setup after loading the view.
     }
     
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        
+        storyBrain.nextStory(userChoice: userAnswer ?? "")
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.story[storyBrain.storyNumber].title
+        
+        choice1Button.setTitle(storyBrain.story[storyBrain.storyNumber].choice1, for: .normal)
+        
+        choice2Button.setTitle(storyBrain.story[storyBrain.storyNumber].choice2, for: .normal)
+    }
 
 
 }
